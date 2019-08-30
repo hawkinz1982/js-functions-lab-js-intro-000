e0ed8c4c43d98bec299908078e48687a93fc3994
Aleksei Lesnikov <alekseilesnikov@gmail.com>
2019-08-30 23:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(Independence Day, you){
-  return 'Happy ${Independence Day}, ${you}!'
+function happyHolidayTo(wedding, Alex){
+  return 'Happy ${wedding}, ${you}!'
 }
