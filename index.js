6690919407cf7ae8f9df68c203e90340c379b252
Aleksei Lesnikov <alekseilesnikov@gmail.com>
2019-08-31 00:17:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(IndependenceDay, Alex){
   return `Happy ${IndependenceDay}, ${Alex}!`
 }
-function holidayCountdown(IndependenceDay, fourdays){
+function holidayCountdown(IndependenceDay, days){
   return `It's ${days} days until ${holiday}!`
 }
